8e546e55c50b5179212fe83d73242cb90cb3c409
Philipp Deppenwiese <zaolin@das-labor.org>
2020-06-08 21:37:46 +0200

    Move to godep, additional tests

--- cmd/sealtotp/main.go
+++ cmd/sealtotp/main.go
@@ -4,15 +4,22 @@ import (
 	"log"
 
 	"github.com/u-root/tpmtotp/pkg/token"
+	"github.com/u-root/u-root/pkg/tss"
 )
 
 const ()
 
 func main() {
-	_, qrImage, err := token.CreateQRSecretHOTP()
+	tpm, err := tss.NewTPM()
 	if err != nil {
 		log.Fatalf("%v\n", err)
 	}
-	token.PrintTotpInAnsii(qrImage)
-
+	_, _, err = token.CreateQRSecretHOTP()
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	err = tpm.TakeOwnership("", "")
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
 }
